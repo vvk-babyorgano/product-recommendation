ffb5b46b6aeac7f4922f9b6b5a3a48f310fb2e6d
vvk-babyorgano <vivek@babyorgano.in>
2024-11-08 18:00:24 +0530

    Change few

--- app/routes/quiz.jsx
+++ app/routes/quiz.jsx
@@ -1,12 +1,8 @@
-import ProductQuiz from "../components/ProductQuiz";
-
-const QuizPage = () => {
-  return (
-    <div>
-      <h1>Product Recommendation Quiz</h1>
-      <ProductQuiz />
-    </div>
-  );
-};
-
-export default QuizPage;
+export default function QuizPage() {
+    return (
+      <div>
+        <h1>Product Recommendation Quiz</h1>
+        <ProductQuiz />
+      </div>
+    );
+  }  
