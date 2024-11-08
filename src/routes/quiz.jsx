e6852b0676baed5f73a2bc7ce133a5cd9fa0d89a
vvk-babyorgano <vivek@babyorgano.in>
2024-11-08 15:21:14 +0530

    Add the routes file

---
+++ src/routes/quiz.jsx
@@ -0,0 +1,12 @@
+import ProductQuiz from "../components/ProductQuiz";  // Adjust the path if necessary
+
+const QuizPage = () => {
+  return (
+    <div>
+      <h1>Product Recommendation Quiz</h1>
+      <ProductQuiz />
+    </div>
+  );
+};
+
+export default QuizPage;
